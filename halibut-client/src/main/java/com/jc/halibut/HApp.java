aa74fbf9c4b7ff0fbb9119589ea3b3a64f881a73
murrkot <andrii.shapovalov.devp@gmail.com>
2026-03-01 00:00:31 +0200

    GWT module renamed

--- halibut-client/src/main/java/com/jc/halibut/HApp.java
+++ halibut-client/src/main/java/com/jc/halibut/HApp.java
@@ -20,7 +20,7 @@ import com.google.gwt.user.client.ui.VerticalPanel;
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
  */
-public class halibutapp implements EntryPoint {
+public class HApp implements EntryPoint {
 	/**
 	 * The message displayed to the user when the server cannot be reached or
 	 * returns an error.
